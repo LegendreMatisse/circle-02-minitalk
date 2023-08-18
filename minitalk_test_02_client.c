51905bde42c2629fef6b3466f7a72e12c806ed9e
Matisse Legendre <mlegendr@a1-r2-p7.s19.be>
2023-08-18 20:32:41 +0200

    Progress Fri 18 Aug 2023 08:32:41 PM CEST

--- minitalk_test_02_client.c
+++ minitalk_test_02_client.c
@@ -6,7 +6,7 @@
 /*   By: mlegendr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 17:12:08 by mlegendr          #+#    #+#             */
-/*   Updated: 2023/08/18 19:15:10 by mlegendr         ###   ########.fr       */
+/*   Updated: 2023/08/18 20:31:59 by mlegendr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,51 @@
 #include <stdlib.h>
 #include "libft/libft.h"
 #include "printf/ft_printf.h"
+
+void	ft_test_client(int pid, char *str)
+{
+	unsigned char	c;
+	int				i;
+	int				bit_count;
+	int				msb_value;
+
+	i = 0;
+	while (str[i])
+	{
+		bit_count = 8;
+		c = str[i];
+		i++;
+		while (bit_count--)
+		{
+			msb_value = 0;
+			msb_value = (c >> bit_count) & 1;
+			if (msb_value == 1)
+			{
+				kill(pid, SIGUSR1);
+				ft_putchar_fd('1', 1);
+			}
+			else if (msb_value == 0)
+			{
+				kill(pid, SIGUSR2);
+				ft_putchar_fd('0', 1);
+			}
+			usleep(500);
+		}
+		ft_putchar_fd('\n', 1);
+	}
+	bit_count = 8;
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 3 || !ft_strlen(argv[2]))
+	{
+		ft_printf("This is not the correct input.\n");
+		ft_printf("Try: ./client <server pid> <string to send>\n");
+		return (1);
+	}
+	ft_test_client(ft_atoi(argv[1]), argv[2]);
+	while (1)
+		pause();
+	return (0);
+}
--- minitalk_test_02_server.c
+++ minitalk_test_02_server.c
@@ -6,7 +6,7 @@
 /*   By: mlegendr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 17:12:08 by mlegendr          #+#    #+#             */
-/*   Updated: 2023/08/18 19:15:38 by mlegendr         ###   ########.fr       */
+/*   Updated: 2023/08/18 20:21:54 by mlegendr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,49 @@
 #include <stdlib.h>
 #include "printf/ft_printf.h"
 #include "libft/libft.h"
+
+static void	ft_test_server(int sig, siginfo_t *info, void *context)
+{
+	static int				i = 7;
+	static int				j = 0;
+	static unsigned char	c = 0;
+
+	(void)info;
+	(void)context;
+	if (sig == SIGUSR1)
+	{
+		ft_putchar_fd('1', 1);
+		c |= (1 << (7 - i));
+	}
+	else if (sig == SIGUSR2)
+	{
+		ft_putchar_fd('0', 1);
+		c |= (0 << (7 - i));
+	}
+	i--;
+	if (++j == 8)
+	{
+		ft_putstr_fd("c: ", 1);
+		ft_putchar_fd(c, 1);
+		ft_putchar_fd('\n', 1);
+		c = 0;
+		i = 7;
+		j = 0;
+	}
+
+}
+
+int	main(void)
+{
+	struct sigaction	s_sa;
+
+	ft_printf("The server has started...\n");
+	ft_printf("The server PID is: %d\n", getpid());
+	s_sa.sa_sigaction = ft_test_server;
+	s_sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &s_sa, 0);
+	sigaction(SIGUSR2, &s_sa, 0);
+	while (1)
+		pause();
+	return (0);
+}
